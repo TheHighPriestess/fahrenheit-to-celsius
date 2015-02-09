50593431e486a81cf923a443502f993c46ed2f7c
TheHighPriestess <elizabeth.hatleli@gmail.com>
2015-02-09 19:34:45 +0200

    My Commit Message

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
 $().ready(function(){
 	var userInput;
 
-	$("#myform").submit(function(event){
+//********GET FAHRENHEIT TEMP TO CONVERT*********//	
+
+	$("#f_to_c").submit(function(event){
 		event.preventDefault();
-		userInput = $("#tempToConvert").val();
+		userInput = $("#fTempToConvert").val();
 		calculateCelsius(userInput);
 	});
+
+//********GET CELSIUS TEMP TO CONVERT*********//
+
+	$("#c_to_f").submit(function(event){
+		event.preventDefault();
+		userInput = $("#cTempToConvert").val();
+		calculateFahrenheit(userInput);
+	});
+
 
 //********CONVERTS FROM FAHRENHEIT TO CELSIUS*********//
 
 	function calculateCelsius (userInput){
 		var answer = (userInput - 32)*(.5556);
 		var roundedAnswer = Math.round(answer);
-		$("#convertedTemp").html(roundedAnswer + "C");
+		$("#convertedfTemp").html(roundedAnswer + "C");
+	}
+
+//********CONVERTS FROM CELSIUS TO FAHRENHEIT*********//
+
+	function calculateFahrenheit (userInput){
+		var answer = (userInput * 1.8) + 32;
+		var roundedAnswer = Math.round(answer);
+		$("#convertedcTemp").html(roundedAnswer + "F");
 	}
 
 });
